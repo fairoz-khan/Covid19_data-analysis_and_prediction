8fd571bc7da7a1a8b107165e6f61f3b32e30b1d8
fairoz-khan <37231481+fairoz-khan@users.noreply.github.com>
2020-07-10 16:00:04 +0530

    Add files via upload

--- world.py
+++ world.py
@@ -331,4 +331,4 @@ def displayClick(btn1, btn2, btn3):
         return (wrld.pie_for_all('Confirmed'))
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8051)
+    app.run_server(debug=False, port=8051)
